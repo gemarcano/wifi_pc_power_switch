dd509ef612565d186039ea9dc384e84e88aad12d
Gabriel Marcano <gabemarcano@yahoo.com>
2023-12-19 16:44:23 -0800

    Fix FreeRTOS malloc data initialization
     - Allow malloc locks to be initialized on demand, previous
       implementation ran into C++ static init fiasco
     - Update FreeRTOS Idle memory management to match latest upstream
       changes

--- src/FreeRTOS_support.cpp
+++ src/FreeRTOS_support.cpp
@@ -2,34 +2,53 @@
 #include <semphr.h>
 #include <malloc.h>
 
-struct malloc_mutex_
+class malloc_mutex
 {
-	StaticSemaphore_t mutex_memory;
+public:
 	SemaphoreHandle_t mutex;
-	malloc_mutex_()
+	static malloc_mutex& get()
+	{
+		static malloc_mutex singleton;
+		return singleton;
+	}
+
+private:
+	StaticSemaphore_t mutex_memory;
+
+	malloc_mutex()
 	{
 		mutex = xSemaphoreCreateRecursiveMutexStatic(&mutex_memory);
 	}
 };
 
-static malloc_mutex_ mutex;
-
 extern "C"
 {
 	void __malloc_lock(struct _reent *ptr)
 	{
+		static malloc_mutex& mutex = malloc_mutex::get();
 		xSemaphoreTakeRecursive(mutex.mutex, portMAX_DELAY);
 	}
 
 	void __malloc_unlock(struct _reent *ptr)
 	{
+		static malloc_mutex& mutex = malloc_mutex::get();
 		xSemaphoreGiveRecursive(mutex.mutex);
 	}
 
-	void vApplicationGetIdleTaskMemory(StaticTask_t **idle_task_tcb, StackType_t **idle_task_stack, uint32_t *idle_stack_size, BaseType_t core_id)
+	void vApplicationGetIdleTaskMemory(StaticTask_t **idle_task_tcb, StackType_t **idle_task_stack, uint32_t *idle_stack_size)
 	{
-		static StaticTask_t task_tcb[configNUMBER_OF_CORES];
-		static StackType_t task_stack[configNUMBER_OF_CORES][configMINIMAL_STACK_SIZE];
+		static StaticTask_t task_tcb;
+		static StackType_t task_stack[configMINIMAL_STACK_SIZE];
+
+		*idle_task_tcb = &task_tcb;
+		*idle_task_stack = task_stack;
+		*idle_stack_size = sizeof(task_stack)/sizeof(*task_stack);
+	}
+
+	void vApplicationGetPassiveIdleTaskMemory(StaticTask_t **idle_task_tcb, StackType_t **idle_task_stack, uint32_t *idle_stack_size, BaseType_t core_id)
+	{
+		static StaticTask_t task_tcb[configNUMBER_OF_CORES - 1];
+		static StackType_t task_stack[configNUMBER_OF_CORES - 1][configMINIMAL_STACK_SIZE];
 
 		*idle_task_tcb = &task_tcb[core_id];
 		*idle_task_stack = task_stack[core_id];
